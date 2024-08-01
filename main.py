6389adeab248f0df262c30b3033e1518dbf61d87
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-02 01:22:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import telebot
-from gpt import gpt
 import os
+from gpt import gpt
 
-bot = telebot.TeleBot(os.environ['TOKEN'])
+# الحصول على توكن البوت من المتغير البيئي
+TOKEN = os.getenv('TOKEN')
+if not TOKEN:
+    raise ValueError("لم يتم تعيين متغير البيئة 'TOKEN'.")
+
+# إنشاء بوت Telegram
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,'✎┊‌ اهلا بك في بوت الذكاء الاصطناعي الخاص بسورس العقرب يمكنك طرح أي سؤال أو خدمة وسيتم الإجابة عنها إن شاء الله 😁\n\nتم الصنيع بواسطة :\nالمطور [𝗠𝗼𝗵𝗮𝗺𝗲𝗱](t.me/Zo_r0 ) \nالمطور [𝗔𝗹𝗹𝗼𝘂𝘀𝗵](t.me/I_e_e_l ) ', parse_mode='Markdown', disable_web_page_preview=True)
-
+    start_message = (
+        '✎┊‌ اهلا بك في بوت الذكاء الاصطناعي الخاص بسورس العقرب '
+        'يمكنك طرح أي سؤال أو خدمة وسيتم الإجابة عنها إن شاء الله 😁\n\n'
+        'تم الصنيع بواسطة :\n'
+        'المطور [𝗠𝗼𝗵𝗮𝗺𝗲𝗱](t.me/Zo_r0) \n'
+        'المطور [𝗔𝗹𝗹𝗼𝘂𝘀𝗵](t.me/I_e_e_l)'
+    )
+    bot.send_message(message.chat.id, start_message, parse_mode='Markdown', disable_web_page_preview=True)
 
 @bot.message_handler(content_types=['text'])
 def gptMessage(message):
-    resp = gpt(message)
-    bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
+    try:
+        # إرسال الرسالة إلى دالة gpt واستلام الرد
+        resp = gpt(message.text)
+        bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
+    except Exception as e:
+        # التعامل مع الأخطاء وإرسال رسالة تنبيهية
+        bot.send_message(message.chat.id, f'حدث خطأ: {e}', parse_mode='HTML')
 
+# بدء الاستماع للرسائل
 bot.infinity_polling()

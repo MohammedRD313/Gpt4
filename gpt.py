36bf92a60ea65004eceba97d294566697237a3eb
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-06 01:37:27 +0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,12 +1,8 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import pytgpt.phind
 
-# تحميل النموذج والمحول
-model_name = "EleutherAI/gpt-j-6B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+bot = pytgpt.phind.PHIND()
 
 def gpt(message):
-    inputs = tokenizer.encode(message, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response
+    return bot.chat(f'{message}')
+
+

573b8edc0e3c69d1d6cd58920ba0bc861f6b92da
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-06 01:36:16 +0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # تحميل النموذج والمحول
 model_name = "EleutherAI/gpt-j-6B"
@@ -6,19 +6,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
 def gpt(message):
-    # ترميز الرسالة
     inputs = tokenizer.encode(message, return_tensors='pt')
-
-    # توليد الاستجابة
     outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
-
-    # فك ترميز الاستجابة
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
     return response
-
-# اختبار الدالة
-if __name__ == "__main__":
-    user_message = "Hello, how are you?"
-    response = gpt(user_message)
-    print("Response:", response)

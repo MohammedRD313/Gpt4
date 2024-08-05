61952355dc8a5d0c4acec91b3d5d7f5ad603f707
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-06 01:33:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import telebot
 import os
-from gpt import gpt
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# تحميل النموذج والمحول
+model_name = "EleutherAI/gpt-j-6B"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+def gpt(message):
+    inputs = tokenizer.encode(message, return_tensors='pt')
+    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
 
 # الحصول على توكن البوت من المتغير البيئي
 TOKEN = os.getenv('TOKEN')

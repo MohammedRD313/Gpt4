a918cdfed8fca503dd45d037bd96c5a67fa02c92
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-06 01:23:04 +0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,15 +1,15 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT4LMHeadModel, GPT4Tokenizer
 
 # تحميل النموذج والمحول
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = GPT4Tokenizer.from_pretrained('gpt-4')
+model = GPT4LMHeadModel.from_pretrained('gpt-4')
 
 def gpt(message):
     # ترميز الرسالة
     inputs = tokenizer.encode(message, return_tensors='pt')
 
     # توليد الاستجابة
-    outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
+    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
 
     # فك ترميز الاستجابة
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)

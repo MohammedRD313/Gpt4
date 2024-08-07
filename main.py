aafbda0b6868634534578a4dc52829180082ca16
Mohamed Ridha <hamodeygame45@gmail.com>
2024-08-08 00:33:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ TOKEN = os.getenv('TOKEN')
 if not TOKEN:
     raise ValueError("لم يتم تعيين متغير البيئة 'TOKEN'.")
 
+# معرف القناة
+CHANNEL_USERNAME = '@Scorpion_scorp'
+
 # إنشاء بوت Telegram
 bot = telebot.TeleBot(TOKEN)
 
@@ -21,15 +24,26 @@ def start(message):
     )
     bot.send_message(message.chat.id, start_message, parse_mode='Markdown', disable_web_page_preview=True)
 
+def check_membership(user_id):
+    try:
+        member = bot.get_chat_member(CHANNEL_USERNAME, user_id)
+        return member.status in ['member', 'administrator', 'creator']
+    except:
+        return False
+
 @bot.message_handler(content_types=['text'])
 def gptMessage(message):
-    try:
-        # إرسال الرسالة إلى دالة gpt واستلام الرد
-        resp = gpt(message.text)
-        bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
-    except Exception as e:
-        # التعامل مع الأخطاء وإرسال رسالة تنبيهية
-        bot.send_message(message.chat.id, f'حدث خطأ: {e}', parse_mode='HTML')
+    user_id = message.from_user.id
+    if not check_membership(user_id):
+        bot.send_message(message.chat.id, f'عذراً، يجب عليك الاشتراك في القناة أولاً: {CHANNEL_USERNAME}', parse_mode='HTML')
+    else:
+        try:
+            # إرسال الرسالة إلى دالة gpt واستلام الرد
+            resp = gpt(message.text)
+            bot.send_message(message.chat.id, f'<b>العقرب : {resp}</b>', parse_mode='HTML')
+        except Exception as e:
+            # التعامل مع الأخطاء وإرسال رسالة تنبيهية
+            bot.send_message(message.chat.id, f'حدث خطأ: {e}', parse_mode='HTML')
 
 # بدء الاستماع للرسائل
 bot.infinity_polling()
